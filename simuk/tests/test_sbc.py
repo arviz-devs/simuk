f7b442a51b8387719213022a18aa3616c9869292
rohanbabbar04 <rohanbabbar0408@gmail.com>
2025-03-15 17:39:14 +0530

    Update numpyro

--- simuk/tests/test_sbc.py
+++ simuk/tests/test_sbc.py
@@ -1,8 +1,11 @@
 import bambi as bmb
 import numpy as np
+import numpyro
+import numpyro.distributions as dist
 import pandas as pd
 import pymc as pm
 import pytest
+from numpyro.infer import NUTS
 
 import simuk
 
@@ -29,6 +32,26 @@ def test_sbc(model):
         model,
         num_simulations=10,
         sample_kwargs={"draws": 5, "tune": 5},
+    )
+    sbc.run_simulations()
+    assert "prior_sbc" in sbc.simulations
+
+
+def test_sbc_numpyro():
+    y = np.array([28.0, 8.0, -3.0, 7.0, -1.0, 1.0, 18.0, 12.0])
+    sigma = np.array([15.0, 10.0, 16.0, 11.0, 9.0, 11.0, 10.0, 18.0])
+
+    def eight_schools_cauchy_prior(J, sigma, y=None):
+        mu = numpyro.sample("mu", dist.Normal(0, 5))
+        tau = numpyro.sample("tau", dist.HalfCauchy(5))
+        with numpyro.plate("J", J):
+            theta = numpyro.sample("theta", dist.Normal(mu, tau))
+        numpyro.sample("y", dist.Normal(theta, sigma), obs=y)
+
+    sbc = simuk.SBC(
+        NUTS(eight_schools_cauchy_prior),
+        data_dir={"J": 8, "sigma": sigma, "y": y},
+        sample_kwargs={"num_warmup": 1000, "num_samples": 1000},
     )
     sbc.run_simulations()
     assert "prior_sbc" in sbc.simulations
